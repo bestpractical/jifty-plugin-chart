a68c70158960726500216cb487dcefab735fede8
Alex Vandiver <alexmv@bestpractical.com>
2009-03-26 03:00:04 +0000

    Bump VERSION of previously-core'd modules, to higher than the core they were bundled with
    git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/plugins/Jifty-Plugin-Chart@6705 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/Plugin/Chart.pm
+++ lib/Jifty/Plugin/Chart.pm
@@ -4,7 +4,7 @@ use warnings;
 package Jifty::Plugin::Chart;
 use base qw/ Jifty::Plugin Class::Accessor::Fast /;
 
-our $VERSION = '0.01';
+our $VERSION = '0.9';
 
 use Jifty::Plugin::Chart::Web;
 use Scalar::Util qw/ blessed /;
